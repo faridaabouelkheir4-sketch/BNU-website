b2f93f3301fc0b682260009796d11a2c55e289ad
Amr khaled sayed <amr24030189@cs.bnu.edu.eg>
2026-08-11 05:24:51 +0200

    Add files via upload

---
+++ Scr/data/Colleges.js
@@ -0,0 +1,60 @@
+let Colleges = [
+  {
+    id: 1,
+    name: "Faculty of Medicine",
+    programs: []
+  },
+  {
+    id: 2,
+    name: "Faculty of Dentistry",
+    programs: []
+  },
+  {
+    id: 3,
+    name: "Faculty of Physical Therapy",
+    programs: []
+  },
+  {
+    id: 4,
+    name: "Faculty of Veterinary Medicine",
+    programs: []
+  },
+  {
+    id: 5,
+    name: "Faculty of Engineering",
+    programs: [
+      "Manufacturing and Materials Engineering",
+      "Mechatronics and Automation Engineering",
+      "Communication Systems Engineering",
+      "Biomedical Engineering",
+      "Construction Engineering",
+      "Housing and Design for Communities"
+    ]
+  },
+  {
+    id: 6,
+    name: "Faculty of Computer Science",
+    programs: [
+      "Artificial Intelligence and Machine Learning",
+      "Computational Linguistics",
+      "Data Science",
+      "Software and Applications Development",
+      "Virtual and Augmented Reality"
+    ]
+    
+  },
+  {
+       id: 7,
+    name: "College of Business and Economics",
+    programs: [
+      "International Business & Relations Management",
+      "International Economics & Finance",
+      "Accounting and Business Informatics",
+      "Digital Marketing and E-Business",
+      "Insurance and statistics"
+    ]
+  }
+  
+];
+
+export default Colleges;
